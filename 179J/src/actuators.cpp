5130e1b29c71c8b0c008bd2a560f8fbde4c788e3
PETER <ptronning@gmail.com>
2026-02-05 17:26:28 -0800

    starting the pull

---
+++ 179J/src/actuators.cpp
@@ -0,0 +1,31 @@
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include "../lib/inits_v2.h"
+
+#define NANO_HW_IMPLEMENTATION
+
+int main(void) {
+  nano_init();
+  actuator1_set_speed(0);
+  actuator2_set_speed(0);
+  actuator1_enable(1);
+  actuator2_enable(0);
+ 
+
+  while (1) {
+    // actuator1_set_speed(120);   // try 120 first for reliable movement
+    // sched_delay_ms(2000);
+
+    // actuator1_set_speed(0);
+    // sched_delay_ms(500);
+
+    // actuator1_set_speed(-120);
+    // sched_delay_ms(2000);
+
+    // actuator1_set_speed(0);
+    // sched_delay_ms(1000);
+  }
+}
