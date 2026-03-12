b45d7ab0ef7164f3984112dfe178071e3f2c6d84
Siffy-Code <101999481+Siffy-Code@users.noreply.github.com>
2026-03-11 17:17:54 -0700

    main for nano
    actuators *should* pause while running - running even one actuator removes any benefit of the solar panels

--- 179J/src/main.cpp
+++ 179J/src/main.cpp
@@ -1,39 +1,13 @@
 
 
 #include <avr/io.h>
-#include <util/delay.h>
 #include <stdint.h>
 
-// for the reading
-#include "../include/inits_v2.h"
-#include "../include/printer.h"
-#include "../include/driver.h"
-#include "../include/actuatorControl.h"
-#include "../include/motorLimits.h"
+#include "inits_v2.h"
+#include "printer.h"
+#include "driver.h"
+#include "motorLimits.h"
 
-#define NANO_HW_IMPLEMENTATION
-
-
-void actuTest1(){
-  nano_init();
-
-  actuator1_enable(1);
-  actuator2_enable(1);
-
-  while (1) {
-    actuator1_set_speed(120);   // try 120 first for reliable movement
-    sched_delay_ms(2000);
-
-    actuator1_set_speed(0);
-    sched_delay_ms(500);
-
-    actuator1_set_speed(-120);
-    sched_delay_ms(2000);
-
-    actuator1_set_speed(0);
-    sched_delay_ms(1000);
-  }
-}
 
 int main(void) {
   nano_init();
@@ -41,48 +15,32 @@ int main(void) {
   serialBegin(); // Printer
   i2c_init();     // Sensors
   sensor_state = Start; // State Machine
-  
-  ActuatorControls actuator;
-  actuator.homeMotors(); 
 
-  
-  //serialPrint("Testing\n");
-  //serialPrint("Sensor Tracking Active\n");
-
+  //serialPrint("I Work \n");
+  actuator1_enable(1);
+  actuator2_enable(1);
+  ActuatorControls motionCtl;
+  motionCtl.homeMotors();
   while (1) {
-
     Sensor_Tick(); 
-
-    
-    if(diff > 50){
+    schde_delay_ms(200);
+     if(diff > 50){
       if(L > R){
-        serialPrint("Left is Brighter Right going up\n");
-        A2Up(5);
-        A1Down(5);
+        //serialPrint("Left is Brighter Right going up\n");
+        if(motionCtl.A2POS >= 0){
+          motionCtl.angle(1);
+        }
+        
       } else {
-        serialPrint("Right is Brighter, Left going up\n");
-        A1Up(5);
-        A2Down(5);
+        //serialPrint("Right is Brighter, Left going up\n");
+        if(motionCtl.A1POS >= 0){
+          motionCtl.angle(-1);
+        }
       }
     }
+    else
+    
+    sched_delay_ms(50);
 
-    // need for the reading of it 
-    sched_delay_ms(750);
-
-    /*
-    // State 1: D3 & D5 ON, D6 & D11 OFF
-    OCR0B = 255; // D5
-    OCR2B = 255; // D3
-    OCR0A = 0;   // D6
-    OCR2A = 0;   // D11
-    sched_delay_ms(1000);
-
-    // State 2: D6 & D11 ON, D3 & D5 OFF
-    OCR0B = 0;
-    OCR2B = 0;
-    OCR0A = 255;
-    OCR2A = 255;
-    sched_delay_ms(1000);
-    */
   }
 }
