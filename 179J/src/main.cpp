7bf955ae4ba45bebaf0e3cbb108b95c11b9279c9
Siffy-Code <101999481+Siffy-Code@users.noreply.github.com>
2026-03-11 18:10:39 -0700

    triple check code

--- 179J/src/main.cpp
+++ 179J/src/main.cpp
@@ -23,7 +23,7 @@ int main(void) {
   motionCtl.homeMotors();
   while (1) {
     Sensor_Tick(); 
-    schde_delay_ms(200);
+    sched_delay_ms(200);
      if(diff > 50){
       if(L > R){
         //serialPrint("Left is Brighter Right going up\n");
