c7dc433291e50e2a3f55f1ac9c5f361d12d86ee8
Siffy-Code <101999481+Siffy-Code@users.noreply.github.com>
2026-03-11 17:09:47 -0700

    Restoring github code

--- 179J/include/motorLimits.h
+++ 179J/include/motorLimits.h
@@ -17,11 +17,14 @@
 //60 for 140
 inline constexpr int MMDELAY = 35;
 inline constexpr int ACTSPEED = 61;
-inline constexpr int STEPS = 10;
+inline constexpr int ACT1SPEED = 140;
+inline constexpr int ACT2SPEED = 130;
+//TODO - steps or MMDELAY adjust
+inline constexpr int STEPS = 5;
 inline constexpr int MAXHEIGHT = 50;
 inline constexpr int MINHEIGHT = 0;
-inline constexpr int ACTGAP = 610;
-inline constexpr int THTOZ = 10;
+inline constexpr int ACTGAP = 400;
+inline constexpr int THTOZ = 1;
 //ActuatorTracker must be declared in main.cpp to use functions - it will track
 //the positions of actuators and the panel side at all times
 struct ActuatorControls{
@@ -64,19 +67,36 @@ int SIDE = 0;
             serialPrint("A2POS = ");serialPrintNum(A2POS);serialPrint("\n");
         }
     }
-    void homeMotors(){
-        //turn on actuators and set to max height
-        actuator1_set_speed(-120);
-        actuator2_set_speed(-120);
-        sched_delay_ms(10000);
+    void ATOP(){
+        actuator1_set_speed(-2*ACT1SPEED);
+        actuator2_set_speed(-2*ACT2SPEED);
+        sched_delay_ms(5000);
         actuator1_set_speed(0);
         actuator2_set_speed(0);
         A1POS = MAXHEIGHT;
         A2POS = MAXHEIGHT;
-        //verify internal functions and return to lowest point
-        A1DN(MAXHEIGHT);
-        A2DN(MAXHEIGHT);
+    }
+    void ABOT(){
+        actuator1_set_speed(2*ACT1SPEED);
+        actuator2_set_speed(2*ACT2SPEED);
+        sched_delay_ms(5000);
+        actuator1_set_speed(0);
+        actuator2_set_speed(0);
+        A1POS = MINHEIGHT;
+        A2POS = MINHEIGHT;
         SIDE = 0;
+    }
+    void homeMotors(){
+        //turn on actuators and set to max height
+        ATOP();
+        ABOT();
+    }
+    void calibrationSequence(){
+        homeMotors();
+        hardAngle(1);
+        hardAngle(0);
+        hardAngle(-1);
+        hardAngle(0);
     }
     //move actuator 1 up by X mm (max 50)
     void A1UP(int mm){
@@ -145,7 +165,8 @@ int SIDE = 0;
     //angle function - 0 degrees is flat pointing up, max
     //angle is 1.65 degrees with 50mm x 34" right triangle
 
-    //this should be stepped by the sensor readings
+    //INTEGRATION FUNCTION: this should be stepped by the sensor readings
+    //enter "1" or "-1" in a while loop comparing sensors for proper use
     //for example, the sensor should read "L greater than R"
     //feed a small value into the function, let it run
     //then check if it needs to run again (stepping)
@@ -183,6 +204,7 @@ int SIDE = 0;
         else{//light is coming from the side with A2 UP
             //run actuator X to top and Y to bottom
             if(SIDE == 0){
+                
                 hardAngle(-2);
             }
             else if(SIDE == -1){
@@ -210,51 +232,76 @@ int SIDE = 0;
     //-1 will move A2 up or down first
     //1 will move A1 up or down first
     //0 is center, 2s are half angle
+    //do not call this function in integrated code
     void hardAngle(int degrees){
         //determine height to move based on degrees
         //float height = fabs(ACTGAP * tan(degrees * M_PI/180));
         //run the A#UP/DN functions here
         if(degrees == 0){
             //run both actuators to bottom limit
-            A1DN(MAXHEIGHT);
-            A2DN(MAXHEIGHT);
-            SIDE = 0;
-            A1POS = MINHEIGHT;
-            A2POS = MINHEIGHT;
+            dualMotor(MAXHEIGHT,2,1,2,1);
         }
         else if(degrees == 1){
-            //run actuator X to top and Y to bottom
-            A1UP(MAXHEIGHT);
-            A2DN(MAXHEIGHT);
-            SIDE = 1;
-            A1POS = MAXHEIGHT;
-            A2POS = MINHEIGHT;
+            dualMotor(MAXHEIGHT,-1,1,1,1);
         }
         else if(degrees == 2){
-            //run actuator X to top and Y to bottom
-            A1UP(MAXHEIGHT/2);
-            A2DN(MAXHEIGHT);
-            SIDE = 1;
-            A2POS = MINHEIGHT;
-
+            dualMotor(MAXHEIGHT,-2,2,2,1);
         }
         else if(degrees == -2){
-            //run actuator X to top and Y to bottom
-            A2UP(MAXHEIGHT/2);
-            A1DN(MAXHEIGHT);
-            SIDE = -1;
-            A1POS = MINHEIGHT;
+            dualMotor(MAXHEIGHT,2,1,-2,2);
         }
-        else{
-            //run actuator X to bottom and Y to top
-            A2UP(MAXHEIGHT);
-            A1DN(MAXHEIGHT);
-            SIDE = -1;
-            A1POS = MINHEIGHT;
-            A2POS = MAXHEIGHT;
+        else if(degrees == -1){
+            dualMotor(MAXHEIGHT,1,1,-1,1);
         }
     }
-
+    //do not call for integration, replaces code in hardAngle
+    //mm, A1neg, A1div, A2neg, A2div
+    //universal motor function - set A1neg or A2neg to 0 to cancel movement
+    //set to -1 to go up, set to 1 to go down, set higher than 1 for faster
+    //set A1div and A2div to slow a particular motor speed
+    //feed mm the desired distance - give it small amounts unless doing hard angles
+    void dualMotor(int mm, int A1neg, int A1div, int A2neg, int A2div){
+        //we don't put the panel all the way up, only all the way down
+        if(A1neg < 0 && A2neg < 0){
+            return;
+        }
+        if(A1div == 0 || A2div == 0 || mm == 0){
+            return;
+        }
+        int calcSteps = STEPS*mm;
+        if(A1neg < -1 && A2neg > 1){
+            calcSteps = calcSteps / A2neg;
+        }
+        else if(A2neg < -1 && A1neg > 1){
+            calcSteps = calcSteps / A1neg;
+        }
+        else if(A1neg > 1 && A2neg > 1){
+            calcSteps = calcSteps / A1neg;
+        }
+        for(int i=0;i<calcSteps;i++){
+                actuator1_set_speed(A1neg * (ACT1SPEED/A1div));
+                actuator2_set_speed(A2neg * (ACT2SPEED/A2div));
+                sched_delay_ms(MMDELAY);
+                actuator1_set_speed(0);
+                actuator2_set_speed(0);
+            }
+            if(A1neg < 0 && A2neg >= 0){
+                SIDE = 1;
+                A2POS = MINHEIGHT;
+                A1POS = MAXHEIGHT/A1div;
+            }
+            else if(A2neg < 0 && A1neg >= 0){
+                SIDE = -1;
+                A1POS = MINHEIGHT;
+                A2POS = MAXHEIGHT/A2div;
+            }
+            else{
+                SIDE = 0;
+                A1POS = MINHEIGHT;
+                A2POS = MINHEIGHT;
+            }
+            
+    }
 
 
 };
